7b8ca34c32ea43e10437cff48492864e1009ca00
mahadevr2000 <mahadev.r@benzyinfotech.com>
2024-05-10 18:55:04 +0530

    budjet

--- src/app/modules/recipes-page/services/recipe-page.service.ts
+++ src/app/modules/recipes-page/services/recipe-page.service.ts
@@ -7,19 +7,19 @@ import { Injectable } from '@angular/core';
 export class RecipePageService {
 
   constructor(private http : HttpClient) {}
-  getRecipeData(slug : string){
-    const headers = new HttpHeaders({
-      'X-RapidAPI-Key': 'cd2d3af89dmshf0c679291c77752p1845fbjsn079977aa0cdd',
-      'X-RapidAPI-Host': 'tasty-co.p.rapidapi.com',
-    });
+  // getRecipeData(slug : string){
+  //   const headers = new HttpHeaders({
+  //     'X-RapidAPI-Key': 'cd2d3af89dmshf0c679291c77752p1845fbjsn079977aa0cdd',
+  //     'X-RapidAPI-Host': 'tasty-co.p.rapidapi.com',
+  //   });
 
-    const url = `https://tasty-co.p.rapidapi.com/recipes/detail?slug=${slug}`;
+  //   const url = `https://tasty-co.p.rapidapi.com/recipes/detail?slug=${slug}`;
 
-    return this.http.get(url, { headers })
-  }
-  // getRecipeData(slug: string){
-  //   return this.http.get('assets/json/sampleRecipe.json')
+  //   return this.http.get(url, { headers })
   // }
+  getRecipeData(slug: string){
+    return this.http.get('assets/json/sampleRecipe.json')
+  }
   getComilationData(slug : string){
 
   }
